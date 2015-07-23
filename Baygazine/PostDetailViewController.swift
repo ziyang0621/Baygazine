f5b946a881a6e8ec94c8eaac356cecdd97bfd1b7
ziyang <ziyang0621@gmail.com>
2015-07-23 10:25:36 -0700

    modified stretchy header

--- Baygazine/PostDetailViewController.swift
+++ Baygazine/PostDetailViewController.swift
@@ -14,25 +14,22 @@ class PostDetailViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var headerImageView: UIImageView!
-    @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var headerImageViewHeightLayoutConstraint: NSLayoutConstraint!
-    @IBOutlet weak var gradientView: GradientView!
+    @IBOutlet weak var headerImageViewTopLayoutContraint: NSLayoutConstraint!
     @IBOutlet weak var titleLabel: UILabel!
     var post: Post?
     var thumbnailImage: UIImage?
     var headerViewFrame: CGRect!
     var maximumStretchHeight: CGFloat?
-    var headerImageViewHeight: CGFloat = 0
-    var previousHeight: CGFloat = 0
     var didLayoutSubviews = false
+    var headerImageViewFrame: CGRect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.delegate = self
-        headerViewFrame = headerView.frame
         headerImageView.clipsToBounds = true
-        headerImageViewHeight = CGRectGetHeight(headerImageView.frame)
+        headerImageViewFrame = headerImageView.frame
         maximumStretchHeight = CGRectGetWidth(scrollView.bounds)
         
         loadArticle()
@@ -61,34 +58,26 @@ class PostDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func updateHeaderView() {
+    func updateHeaderImageView() {
         let insets = scrollView.contentInset
         let offset = scrollView.contentOffset
         let minY = -insets.top
         if offset.y < minY {
             let deltaY = fabs(offset.y - minY)
-            var frame = headerViewFrame
+            var frame = headerImageViewFrame
             frame.size.height = min(max(minY, kHeaderViewHeight + deltaY), maximumStretchHeight!)
             frame.origin.y = CGRectGetMinY(frame) - deltaY
-            headerView.frame = frame
-            
-            if (previousHeight != CGRectGetHeight(frame)) {
-                headerImageViewHeightLayoutConstraint.constant = headerImageViewHeight + deltaY
-                previousHeight = CGRectGetHeight(frame)
-            }
-            
-            println("\(gradientView.frame) \(headerImageView.frame) \(headerView.frame)")
+            headerImageViewTopLayoutContraint.constant = frame.origin.y
+            headerImageViewHeightLayoutConstraint.constant = frame.size.height
         }
-
     }
-
     
 }
 
 extension PostDetailViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(scrollView: UIScrollView) {
         if didLayoutSubviews {
-            updateHeaderView()
+            updateHeaderImageView()
         }
     }
 }
